79fb11fd93c6f0bbbaaddf308d31802e1394863c
Neil Patterson <neil.patterson@finalsite.com>
2024-04-20 10:31:51 -0400

    change my dashboard ascii art and add wilder autosuggest

--- nvim/.config/nvim/lua/neil/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/neil/plugins/treesitter.lua
@@ -1,35 +1,36 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		-- import nvim-treesitter plugin
+		local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      auto_install = true,
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+		-- configure treesitter
+		treesitter.setup({ -- enable syntax highlighting
+			highlight = {
+				enable = true,
+			},
+			-- enable indentation
+			indent = { enable = true },
+			-- enable autotagging (w/ nvim-ts-autotag plugin)
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = { "markdown", "markdown_inline" },
+			auto_install = true,
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
---
+++ nvim/.config/nvim/lua/neil/plugins/wilder.lua
@@ -0,0 +1,23 @@
+return {
+	"gelguy/wilder.nvim",
+	config = function()
+		local wilder = require("wilder")
+
+		wilder.set_option(
+			"renderer",
+			wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+				highlighter = wilder.basic_highlighter(),
+				min_width = "100%", -- minimum height of the popupmenu, can also be a number
+				min_height = "50%", -- to set a fixed height, set max_height to the same value
+				reverse = 0,
+				-- 'single', 'double', 'rounded' or 'solid'
+				-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+				border = "rounded",
+			}))
+		)
+
+		wilder.setup({
+			modes = { ":", "/", "?" },
+		})
+	end,
+}
