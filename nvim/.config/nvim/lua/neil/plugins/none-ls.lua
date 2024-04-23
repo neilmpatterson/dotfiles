df1c957fce20c9c21797acfb8c45914a16bb0bd3
Neil Patterson <neil.patterson@finalsite.com>
2024-04-23 15:30:15 -0400

    add in bash and shell script formatting and format my bash scripts

--- nvim/.config/nvim/lua/neil/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/neil/plugins/none-ls.lua
@@ -9,6 +9,7 @@ return {
 				-- null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh", "zsh" } }),
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {
